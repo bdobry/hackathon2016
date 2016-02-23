ceee354957b73282684fb554eed0f50665814064
Bjarke Mulbjerg <bjarkemulbjerg12@gmail.com>
2016-02-23 23:04:27 +0100

    Added a different background color overlay, changed width of forms and some more styling

--- registerpage.php
+++ registerpage.php
@@ -2,15 +2,17 @@
 	echo file_get_contents("head.php");
 	?>
 
-<img src="images/blue-bg.jpg" alt="Background image" class="bg">
+<img src="images/grey-bg.jpg" alt="Background image" class="bg">
 	<div class="container">
 		<header class="col-sm-12"><img class="img-responsive center-block lp-logo" src="http://placehold.it/200x200" alt=""></header>
 		<h1 class="whitetxt text-center">Welcome to Classmate</h1>
 		<p class="whitetxt text-center">Classmate is a student-driven community, made by students - for students</p>
 		<div class="col-sm-12 lp-forms center-block">
-			<?php
-			echo file_get_contents("register-card.php");
-			?>
+			<div class="form-container col-sm-12 center-block">
+				<?php
+				echo file_get_contents("register-card.php");
+				?>
+			</div>
 		</div>
 
 	</div>
