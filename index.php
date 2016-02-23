5c33945649144ae7a428b6a50623540be5a957f9
Bjarke Mulbjerg <bjarkemulbjerg12@gmail.com>
2016-02-23 19:59:39 +0100

    Made login form responsive

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 	<?php 
 	echo file_get_contents("head.php");
+	?>
+
+	<div class="container">
+	<?php
 	echo file_get_contents("login-card.php");
+	?>
+	</div>
+	<?php
 	echo file_get_contents("footer.php");
 	?>
--- login-card.php
+++ login-card.php
@@ -1,18 +1,35 @@
-		<h1>Log in</h1>
-		<br>
-		<form action="login.php" method="post">
-			<input type="text" name="user" placeholder="Username">
-			<input type="password" name="pass" placeholder="Password">
-			<input type="submit" name="login" class="login login-submit" value="Log in">
-		</form>
-		<form action="registerpage.php">
-			<input type="submit" class="login login-submit" value="Register">
-		</form>
-		<div class="login-help">
+<div class="col-sm-12">
+	<h2>Log in</h2>	
+	<form action="login.php" method="post" class="login-form">
+	
+		<div class="form-group">
+			<label for="Username">Username</label>
+			<input type="text" class="form-control" name="user" placeholder="Username">
+		</div>
+	
+		<div class="form-group">
+			<label for="Password">Password</label>
+			<input type="password" class="form-control" name="pass" placeholder="Password">
+		</div>
+		<div class="form-group">
+			<input type="submit" name="login" class="form-control btn btn-success login login-submit" value="Log in">
+		</div>
+	</form>
+	
+	<form class="register-form" action="registerpage.php">
+		<div class="form-group">
+			<input type="submit" class="form-control btn btn-primary login login-submit" value="Register">
+			<label for="No-account">Don't have an account? Register here!</label>
+		</div>
+	</form>
+	<div class="login-help">
 		<?php if(isset($_COOKIE["user"]))
 		{
 			setcookie ("User", "", time() - 3600);
 			echo("No user named ".$_COOKIE["user"]); 
 		}?>
-		</div>
 	</div>
+</div>
+
+
+	
