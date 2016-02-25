a2deee0b4a665eb347048b8c64c7ce5416a9c7e1
Bjarke Mulbjerg <bjarkemulbjerg12@gmail.com>
2016-02-25 22:34:03 +0100

    Logo added to index and register page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <img src="images/grey-bg.jpg" alt="Background image" class="bg">
 	<div class="container">
-		<header class="col-sm-12"><img class="img-responsive center-block lp-logo" src="http://placehold.it/200x200" alt=""></header>
+		<header class="col-sm-12"><img class="img-responsive center-block lp-logo" src="images/pictoriallogo.png" alt=""></header>
 		<h1 class="whitetxt text-center">Welcome to Classmate</h1>
 		<p class="whitetxt text-center">Classmate is a student-driven community, made by students - for students</p>
 		<div class="col-sm-12 lp-forms center-block">
