b15e12c010d36857162a7d45b4c7126c30e3a3ca
Bjarke Mulbjerg <bjarkemulbjerg12@gmail.com>
2016-02-25 09:00:01 +0100

    no message

--- askquestion.php
+++ askquestion.php
@@ -16,40 +16,41 @@
   <hr>
 
 
-<div class="row">
-  <form role="form" action="" method="post" >
-    <div class="col-lg-6">
-      <div class="well well-sm"><strong><i class="glyphicon glyphicon-ok form-control-feedback"></i> Required Field</strong></div>
-      <div class="form-group">
-        <label for="InputName">Title</label>
-        <div class="input-group">
-          <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Enter Title" required>
-          <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
+  <div class="row">
+    <form role="form" action="" method="post" >
+      <div class="col-lg-6">
+        <div class="well well-sm"><strong><i class="glyphicon glyphicon-ok form-control-feedback"></i> Required Field</strong></div>
+        <div class="form-group">
+          <label for="InputName">Title</label>
+          <div class="input-group">
+            <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Enter Title" required>
+            <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
+        </div>
+        <div class="form-group">
+          <label for="InputCategory">Category</label>
+          <div class="input-group">
+            <div class="form-group">
+              <select class="form-control" id="categoryselect">
+                <option value="" selected disabled>Please select</option>
+                <option>Design</option>
+                <option>Code</option>
+                <option>Off topic</option>
+              </select>
+            </div>
+            <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
+        </div>      
+        <div class="form-group">
+          <label for="InputMessage">Question</label>
+          <div class="input-group">
+            <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" placeholder="Write your question here" required></textarea>
+            <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
+        </div>
+        <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-primary pull-right">
       </div>
-      <div class="form-group">
-        <label for="InputCategory">Category</label>
-        <div class="input-group">
-          <div class="form-group">
-            <select class="form-control" id="categoryselect">
-              <option value="" selected disabled>Please select</option>
-              <option>Design</option>
-              <option>Code</option>
-              <option>Off topic</option>
-            </select>
-          </div>
-          <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
-      </div>      
-      <div class="form-group">
-        <label for="InputMessage">Question</label>
-        <div class="input-group">
-          <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" placeholder="Write your question here" required></textarea>
-          <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
-      </div>
-      <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-primary pull-right">
-    </div>
-  </form>
-</div>
+    </form>
+  </div>
 
+  
 </div>
 
         
