a41042e2092e957495ba2de7093d9262b15f10d0
Bjarke Mulbjerg <bjarkemulbjerg12@gmail.com>
2016-02-25 08:57:16 +0100

    Ask question page layout done

--- askquestion.php
+++ askquestion.php
@@ -9,35 +9,48 @@
 
         <!-- Jumbotron Header -->
         <header class="jumbotron fp-jt">
-            <h1>Code questions</h1>
+            <h1>Ask question</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
         </header>
 
         <hr>
 
-        <!-- Title -->
-            <div class="col-sm-12">
-            <div class="container">
+<div class="container">
+<div class="row">
   <form role="form" action="" method="post" >
-    <div class="col-lg-12">
+    <div class="col-lg-6">
       <div class="well well-sm"><strong><i class="glyphicon glyphicon-ok form-control-feedback"></i> Required Field</strong></div>
       <div class="form-group">
-      <div class="form-group">
-        <label for="InputEmail">Your Email</label>
+        <label for="InputName">Title</label>
         <div class="input-group">
-          <input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="Enter Email" required  >
+          <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Enter Title" required>
           <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
       </div>
       <div class="form-group">
-        <label for="InputMessage">Message</label>
-        <div class="input-group"
-          <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" required></textarea>
+        <label for="InputCategory">Category</label>
+        <div class="input-group">
+          <div class="form-group">
+            <select class="form-control" id="categoryselect">
+              <option value="" selected disabled>Please select</option>
+              <option>Design</option>
+              <option>Code</option>
+              <option>Off topic</option>
+            </select>
+          </div>
+          <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
+      </div>      
+      <div class="form-group">
+        <label for="InputMessage">Question</label>
+        <div class="input-group">
+          <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" placeholder="Write your question here" required></textarea>
           <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
       </div>
+      <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-primary pull-right">
+    </div>
   </form>
-  <hr class="featurette-divider hidden-lg">
-  </div>
-            </div>
+</div>
+
+</div>
 
         
 	<?php
