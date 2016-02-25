57da5fb14b4ffcfafa4de224eb3626bda767595b
[nyuu92] <[adrianadebowska@gmail.com]>
2016-02-25 15:58:22 +0100

    layout for adding question

--- add_quiz.php
+++ add_quiz.php
@@ -11,7 +11,7 @@
               
                 <div class="btn-group inline" role="group">
                     
-                    <a href="add_quiz2.php" class="btn btn-primary">Create question</a>
+                    <a href="add_quiz.php" class="btn btn-primary">Create question</a>
                    
                 </div>
                 </div>
