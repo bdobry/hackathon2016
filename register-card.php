d83dcfc5f9f0bf82fb102a6a552f3023e8147c55
Bjarke Mulbjerg <bjarkemulbjerg12@gmail.com>
2016-02-26 08:43:42 +0100

    no message

--- register-card.php
+++ register-card.php
@@ -1,17 +1,17 @@
 <div class="login-card">
 	<h3>Register</h3>
-	<form action="register.php" method="post">
+	<form id="registerForm" action="register.php" method="post">
 		<div class="form-group">
-			<label for="Username">Username</label>
-			<input type="text" class="form-control" name="user" placeholder="Username">
+			<label for="regUsername">Username</label>
+			<input type="text" class="form-control" id="regUsername" name="regUsername" placeholder="Username" required>
 		</div>
 		<div class="form-group">
-			<label for="password">Password</label>
-			<input type="password" class="form-control" name="pass" placeholder="Password">
+			<label for="regUsername">Password</label>
+			<input type="password" class="form-control" name="regPass" id="regPass" placeholder="Password" required>
 		</div>
       <div class="form-group">
-        <label for="InputEmail">Your Email</label>
-          <input type="email" class="form-control" id="InputEmail" name="email" placeholder="Enter Email" required>
+        <label for="regEmail">Your Email</label>
+          <input type="email" class="form-control" id="regEmail" name="regEmail" placeholder="Enter Email" required>
       </div>
 		<div class="form-group">
 			<input type="submit" name="register" class="btn btn-success form-control login login-submit" value="Register">
